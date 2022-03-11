6c991ed325115c6e8605ad4c3d7b76c1eff966f5
Michael DePietto <m.depietto1@gmail.com>
2022-03-10 20:40:43 -0500

    added mui notes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
 import React from 'react'
+
 import Emotion from './components/Emotion'
+
+import Material from './components/Material'
+import { createTheme, ThemeProvider } from '@material-ui/core/styles'
+import { green } from '@material-ui/core/colors'
+
+//changes default theme properties - mui
+const theme = createTheme({
+  palette: {
+      primary: {
+        // original is blue
+          main: green[500]
+      }
+  },
+  typography: {
+    fontFamily: "Smooch Sans",
+    h1: {
+      fontSize: '8rem',
+      fontWeight: 600
+    }
+  }
+})
 
 function App() {
   return (
     <div>
-      <Emotion />
+      {/* <Emotion /> */}
+      <ThemeProvider theme={ theme }>
+        <Material />
+      </ThemeProvider>
     </div>
   );
 }
 
 export default App;
 
-// material ui v4 https://v4.mui.com/
 // react hook form https://react-hook-form.com/
---
+++ src/components/Material.js
@@ -0,0 +1,62 @@
+import React from 'react'
+
+import { Button, IconButton, Typography } from '@material-ui/core'
+import { makeStyles } from '@material-ui/styles'
+import AlarmIcon from '@material-ui/icons/Alarm'
+
+// creates custom class
+const useStyles = makeStyles({
+    mockParagraph: {
+        backgroundColor: '#494944',
+        color: 'red',
+        fontSize: '24px',
+        fontFamily: 'Roboto'
+    }
+})
+
+const Material = () => {
+
+    const classes = useStyles()
+
+    return (
+        <div>
+            <Button
+                variant='contained'
+                color='primary'
+                onClick={ () => alert('ow') }
+            >
+                Press me
+            </Button>
+            <Button
+                variant='text'
+            >
+                Press me
+            </Button>
+            <Button
+                variant='outlined'
+                color='secondary'
+                size='large'
+                fullWidth
+            >
+                Press me
+            </Button>
+            <IconButton>
+                <AlarmIcon />
+            </IconButton>
+            <Typography
+                variant='h1'
+                color='primary'
+            >
+                hello there
+            </Typography>
+            <Typography
+                variant='h6'
+                className={ classes.mockParagraph }
+            >
+                There's a hole in my pants
+            </Typography>
+        </div>
+    )
+}
+
+export default Material
